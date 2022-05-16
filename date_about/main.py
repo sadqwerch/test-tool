98f66b3da269a2cb0feb8c8c93c076373775470b
sadqwerch <43747553+sadqwerch@users.noreply.github.com>
2022-05-16 12:50:05 +0800

    Update main.py

--- date_about/main.py
+++ date_about/main.py
@@ -6,6 +6,16 @@ import numpy as np
 dataset = read_excel('C:\\Users\\sadqwerch\\Desktop\\毕设\\数据.xlsx', sheet_name = 'Sheet5', index_col = 0)
 values = dataset.values
 print(values)
+def Z_Score(data):
+    lenth = len(data)
+    total = sum(data)
+    ave = float(total)/lenth
+    tempsum = sum([pow(data[i] - ave,2) for i in range(lenth)])
+    tempsum = pow(float(tempsum)/lenth,0.5)
+    for i in range(lenth):
+        data[i] = (data[i] - ave)/tempsum
+    return data
+date = Z_score(date)
 data = preprocessing.scale(values)
 print(data)
 X = np.array(data)
